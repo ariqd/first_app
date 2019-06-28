2d806661ec30133606968fe73c7d3f1d98c69635
Ariq Daffa Athallah Putra <ariqdaffaathallah@gmail.com>
2019-06-28 10:43:28 +0700

    initial commit

---
+++ lib/pages/products_admin.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import './product_create.dart';
+import './product_list.dart';
+
+class ProductsAdminPage extends StatelessWidget {
+  final Function addProduct;
+  final Function deleteProduct;
+
+  ProductsAdminPage(this.addProduct, this.deleteProduct);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text('Choose'),
+              ),
+              ListTile(
+                title: Text('All Products'),
+                onTap: () {
+                  Navigator.pushReplacementNamed(context, '/');
+                },
+              )
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Text('Manage Products'),
+          bottom: TabBar(tabs: <Widget>[
+            Tab(
+              text: 'Create Product',
+              icon: Icon(Icons.create),
+            ),
+            Tab(
+              text: 'My Products',
+              icon: Icon(Icons.list),
+            ),
+          ]),
+        ),
+        body: TabBarView(children: <Widget>[
+          ProductCreatePage(addProduct),
+          ProductListPage(),
+        ]),
+      ),
+    );
+  }
+}
